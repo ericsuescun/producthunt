73498450de4d0166b3fc43a0fa03765373df2eb1
Eric Suescun <edsuescun@gmail.com>
2019-03-06 13:45:21 -0500

    Lista la app con todas las funcionalidades

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,8 +25,8 @@ class ProductsController < ApplicationController
 	end
 
 	def update
-		@product = Product.find(params[:id])
-		if @product.update(product_params)
+		product = Product.find(params[:id])
+		if product.update(product_params)
 			redirect_to products_path, notice: "El producto ha sido modificado con éxito"
 		else
 			render :edit
