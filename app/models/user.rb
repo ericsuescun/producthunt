fd94b962af012761caeed20f4ebf392989316ca3
Eric Suescun <edsuescun@gmail.com>
2019-03-06 17:19:18 -0500

    Agrega validaciones a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,9 @@
 
 class User < ActiveRecord::Base
 	has_secure_password validation: false
+
+	validates :email, uniqueness: true, format: /@/
+	validates :password, presence: true, on: :create
+	validates :password, length: { in: 6..20 }, allow_nil: true
+	validates :name, presence: true
 end
